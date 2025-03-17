3a48cd181bcb2f07352fd530a8d5609b3560e475
cubee-chen <128008326+cubee-chen@users.noreply.github.com>
2025-03-17 23:47:14 +0800

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID, // from Google Cloud Console
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: process.env.GOOGLE_REDIRECT_URI,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
